3698e5f11a9ac4ed8064fc012c83d2a237b45ea7
Pascal <pascal.decker@outlook.com>
2018-05-27 16:56:02 +0200

    Logik implementiert, Ausgabe fehlt

--- minimal.cpp
+++ minimal.cpp
@@ -1,6 +1,34 @@
 #include <windows.h>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
 HINSTANCE Instanz;
 LRESULT CALLBACK Fensterfunktion(HWND, UINT, WPARAM, LPARAM);
+
+#define UP_BUTTON 1
+#define DOWN_BUTTON 2
+
+// Prototypes
+void AddMenu(HWND);
+void AddControls(HWND);
+void createDialog(HINSTANCE);
+void displayDialog(HWND);
+int fibVoni(int);
+
+// Window-Handler
+HMENU hMenu;
+HWND hDialog, hintIOut, hFIOut;
+
+// Variables
+int i = 0;
+int output = 0;
+stringstream ss;
+string str;
+
+
+
 //-----------------Windows - Hauptprogramm ------------------------------------
 int WINAPI WinMain(HINSTANCE dieseInstanz,
                    HINSTANCE vorherigeInstanz,
@@ -25,23 +53,15 @@ int WINAPI WinMain(HINSTANCE dieseInstanz,
         wincl.lpszMenuName = NULL;
         wincl.lpszClassName = "Einfach";
         wincl.cbSize = sizeof(WNDCLASSEX);
-        if (!RegisterClassEx(&wincl))
+        
+        if (!RegisterClassEx(&wincl)) {
             return 255;
+        }
+        createDialog(dieseInstanz);
     }
 
-    Hauptfenster = CreateWindowEx(
-        0,
-        "Einfach",
-        "Hello MS Winworld",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        NULL,
-        NULL,
-        dieseInstanz,
-        NULL);
+    Hauptfenster = CreateWindowEx( 0,"Einfach","Fibonacci-Zahlen",WS_OVERLAPPEDWINDOW,CW_USEDEFAULT,CW_USEDEFAULT,800,500,NULL,NULL,dieseInstanz,NULL);
+
     if (!Hauptfenster)
         return 255;
     ShowWindow(Hauptfenster, FensterStil);
@@ -49,12 +69,22 @@ int WINAPI WinMain(HINSTANCE dieseInstanz,
         DispatchMessage(&Meldung);
     return Meldung.wParam;
 } // Ende WinMain
+
 //-----------------Callback Funktion des Hauptfensters ------------------------
 LRESULT CALLBACK Fensterfunktion(HWND Fenster, UINT nachricht, WPARAM wParam,
                                  LPARAM lParam)
 {
     switch (nachricht)
     {
+    case WM_COMMAND:
+        switch(wParam) {
+            case 1:
+            displayDialog(Fenster);
+            break;
+        }
+    case WM_CREATE:
+        AddMenu(Fenster);
+        break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -63,3 +93,90 @@ LRESULT CALLBACK Fensterfunktion(HWND Fenster, UINT nachricht, WPARAM wParam,
     }
     return 0;
 }
+
+//-----------------Callback Funktion des Dialogs ------------------------
+LRESULT CALLBACK DialogFunktion(HWND dialogFenster, UINT nachricht, WPARAM wParam,
+                                 LPARAM lParam)
+{
+    switch (nachricht)
+    {
+        case WM_CLOSE:
+            DestroyWindow(dialogFenster);
+            break;
+        case WM_COMMAND:
+            switch(wParam) {
+                case UP_BUTTON:     
+                output = fibVoni(i);
+                    i++;
+                    break;
+                case DOWN_BUTTON:
+                    i--;
+                break;
+            }
+        default:
+        return DefWindowProcW(dialogFenster,nachricht,wParam,lParam);
+    }
+}
+
+// Add Menübar
+void AddMenu(HWND Fenster) {
+    hMenu = CreateMenu();
+
+    AppendMenu(hMenu, MF_STRING, 1,"Open Dialog");
+    SetMenu(Fenster, hMenu);
+}
+
+// Create Dialog
+void createDialog(HINSTANCE dieseInstanz) {
+    WNDCLASSW dialog = {0};
+        dialog.lpfnWndProc = DialogFunktion;
+        dialog.hInstance = dieseInstanz;
+        dialog.hCursor = LoadCursor(NULL, IDC_CROSS);
+        dialog.hbrBackground = (HBRUSH)COLOR_BACKGROUND;
+        dialog.lpszClassName = L"DialogClass";
+
+        RegisterClassW(&dialog);
+}
+
+
+// Display Dialog
+void displayDialog(HWND dialogFenster) {
+    hDialog = CreateWindowW(L"DialogClass",L"Fibonaccifolge", WS_VISIBLE | WS_OVERLAPPEDWINDOW, 200,200,450,400,dialogFenster,NULL,NULL,NULL);
+
+    AddControls(hDialog);
+}
+
+// Labels
+void AddControls(HWND dialogFenster) {
+    // Header
+    CreateWindowW(L"static",L"int i",WS_VISIBLE | WS_CHILD | SS_CENTER | WS_BORDER, 20,100,50,20,dialogFenster,NULL,NULL,NULL);
+    CreateWindowW(L"static",L"F ( i )",WS_VISIBLE | WS_CHILD | SS_CENTER | WS_BORDER, 20,125,50,20,dialogFenster,NULL,NULL,NULL);
+
+    // Output
+    hintIOut = CreateWindowW(L"edit",L"",WS_VISIBLE | WS_CHILD | WS_BORDER, 80,100,25,20,dialogFenster,NULL,NULL,NULL);
+    hFIOut = CreateWindowW(L"edit",L"",WS_VISIBLE | WS_CHILD | WS_BORDER,  80,125,25,20,dialogFenster,NULL,NULL,NULL);
+
+    // Buttons
+    CreateWindowW(L"button",L"Up",WS_VISIBLE | WS_CHILD | SS_CENTER, 20,170,70,30,dialogFenster,(HMENU) UP_BUTTON,NULL,NULL);
+    CreateWindowW(L"button",L"Down",WS_VISIBLE | WS_CHILD | SS_CENTER, 100,170,70,30,dialogFenster,(HMENU) DOWN_BUTTON,NULL,NULL);
+}
+
+// FibVoni
+int fibVoni(int fib) {
+    
+    if(fib < 0) {
+        //cout << "Bitte eine Zahl größer gleich 0 eingeben!"
+    }
+
+    if(fib == 0) {
+        return 0;
+    }
+    if(fib == 1 || fib == 2) {
+        return 1;
+    }
+    else {
+        return fibVoni(fib-1) + fibVoni(fib-2);
+    }
+}
+
+
